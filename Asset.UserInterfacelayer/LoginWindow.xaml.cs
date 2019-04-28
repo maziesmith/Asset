4f58e7532cd77dc48f9ad02b333293bd3bc1ca74
heyj <1214025414@qq.com>
2019-04-28 17:32:26 +0800

    注册密码录入时显示问题

--- Asset.UserInterfacelayer/LoginWindow.xaml.cs
+++ Asset.UserInterfacelayer/LoginWindow.xaml.cs
@@ -43,6 +43,17 @@ namespace Asset
                       }
                 }
             });
+            mtbUserPassword.TextDecorations = new TextDecorationCollection(new TextDecoration[] {
+                new TextDecoration() {
+                     Location= TextDecorationLocation.Strikethrough,
+                      Pen= new Pen(Brushes.Black, 10f) {
+                          DashCap =  PenLineCap.Round,
+                           StartLineCap= PenLineCap.Round,
+                            EndLineCap= PenLineCap.Round,
+                             DashStyle= new DashStyle(new double[] {0.0,1.2 }, 0.6f)
+                      }
+                }
+            });
             //绑定数据
             BindDrop();
         }
